04363f44c3704764950e7d1d72f5c0d876c80c69
Cam Cope <github@camcope.me>
2021-01-24 04:48:36 -0800

    something that runs but returns empty set

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,98 @@
 #![allow(unused_imports)]
-use std::collections::HashSet;
+use std::{borrow::Borrow, collections::HashSet, fmt::Debug};
+use std::collections::hash_map::RandomState;
 use std::iter::FromIterator;
-use std::sync::RwLock;
-use std::fs::{read_dir, read_to_string};
+use std::fs::{
+    read_dir,
+    read_to_string,
+    write
+};
 use std::fs::{self, DirEntry};
 use std::path::{Path, PathBuf};
 
-use anyhow::anyhow;
+use color_eyre::eyre::{Result, eyre};
+use color_eyre::{eyre::Report, eyre::WrapErr, Section};
 use indicatif::ParallelProgressIterator;
+use parking_lot::RwLock;
 use rayon::prelude::*;
 use serde_json::Value;
-use tracing;
+use tracing::{Level, Subscriber, event, instrument, span};
+use tracing_subscriber;
+
 //use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
+/// Copied from HashSet.is_subset, adapted for refs on left side
+fn is_left_ref_subset(left: &HashSet<&String, RandomState>, right: &HashSet<String, RandomState>) -> bool {
+    if left.len() <= right.len() { left.iter().all(|v: &&String| right.contains(*v)) } else { false }
+}
 
-fn do_work(dir: &Path, mut keys: RwLock<HashSet<String>>) -> anyhow::Result<()> {
-    let paths: Vec<PathBuf> = read_dir(dir)?
+//fn left_not_in_right<'a>(left: &'a HashSet<&T, S>, right: &'a HashSet<T, S>) -> Vec<&'a T> {
+//    *left.filter(|v| !*right.contains(*v)).collect()
+//}
+
+#[instrument]
+fn get_inputs(dir: &Path) -> Result<Vec<PathBuf>> {
+    span!(Level::TRACE, "get_inputs");
+    Ok(read_dir(dir)?
         .map(|f| f.unwrap().path())
-        .collect();
-    let r = paths.into_par_iter()
+        .collect())
+}
+
+#[instrument]
+fn process_inputs(paths: Vec<PathBuf>, keys: &RwLock<HashSet<String>>) {
+    span!(Level::TRACE, "iter_paths");
+    paths.into_par_iter()
         .progress()
         .for_each(|path|
-        match parse(path, &keys) {
-            Ok(_) => (),
-            Err(e) => tracing::error!("fuck"),
+            match parse(&path, &keys) {
+                Ok(_) => (),
+                Err(e) => drop(e.wrap_err(format!("Path: {:?}", &path))),
         }
-    );
+    )
+}
+
+#[instrument]
+fn parse(path: &PathBuf, keys: &RwLock<HashSet<String>>) -> Result<()> {
+    let s = read_to_string(path)?;
+    let json: Value = serde_json::from_str(&s)?;
+    if !json.is_object() {
+        return Err(eyre!("not a map"))
+    }
+    //let new_keys = HashSet::<String>::from_iter(json.as_object().unwrap().keys().cloned());
+    let new_keys = HashSet::<&String>::from_iter(json.as_object().unwrap().keys());
+    if is_left_ref_subset(&new_keys, &keys.read()) {
+        return Ok(())
+    }
+    keys.write().extend(new_keys.iter().map(|x| x.to_string()));
+
     Ok(())
 }
 
-fn parse(path: PathBuf, keys: &RwLock<HashSet<String>>) -> anyhow::Result<Box<Value>, anyhow::Error> {
-    let s = read_to_string(path)?;
-    let json: Value = serde_json::from_str(&s)?;
-    let test = HashSet::<String>::from_iter(vec!("a".to_string(),"b".to_string()).into_iter());
-    if !json.is_object() {
-        return Err(anyhow!("not a map"))
-    }
-    let obj = json.as_object().unwrap().keys();
-    //let new_keys = HashSet::<String>::from_iter();
+fn install_tracing() {
+    use tracing_error::ErrorLayer;
+    use tracing_subscriber::prelude::*;
+    use tracing_subscriber::{fmt, EnvFilter};
 
-    //Ok(Box(new_keys))
-    return Err(anyhow!("not a map"))
+    let fmt_layer = fmt::layer().with_target(false);
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
+
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .with(ErrorLayer::default())
+        .init();
 }
 
-    /* let res: Result<Value, Box<std::Error>> = read_to_string(path)
-         .map_err(Into::into)
-         .and_then(|s| serde_json::from_str(&s))
-         .map_err(Into::into)
-         .and_then(|j| if j.is_object() {
-             Ok(HashSet::from_iter(j.as_object().unwrap().keys()))
-         } else {
-             Err(anyhow!("not a map"))
-         })
-         .and_then(|k| if !k.is_subset(keys.read().unwrap()) {
-             keys.write().unwrap().extend(k);
-         });
-    };
-    match res {
-      Ok(d) => Ok(todo!()),
-      Err(e) => return Err(e)
-    }*/
-    //Ok(())
-
-fn main() {
-    println!("Hello, world!");
-    let mut keys: HashSet<String> = HashSet::new();
-    let mut locked_set = RwLock::new(keys);
+fn main() -> Result<()> {
+    install_tracing();
+    color_eyre::install()?;
+    let keys: HashSet<String> = HashSet::new();
+    let locked_set = RwLock::new(keys);
     let dir = Path::new("./metadata");
-    do_work(&dir, locked_set);
+    let paths = get_inputs(&dir)?;
+    process_inputs(paths, &locked_set);
+    write("result.txt", format!("{:?}", locked_set.read()))?;
+    Ok(())
 }
